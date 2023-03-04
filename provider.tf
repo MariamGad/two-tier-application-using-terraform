191226ef1e8a9b044ed9dab8d30342cf32938ba3
MariamGad <mariamgad142@gmail.com>
2023-03-04 17:51:26 +0200

    change region

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    required_providers {
+        aws={
+            source = "hashicorp/aws"
+            version = "~>4.0"
+        }
     }
-  }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-west-1"
+    region = var.region
 }
