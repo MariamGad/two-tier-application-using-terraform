a065963c22b65cb58771cd70e8b7eee950ce44ce
MariamGad <mariamgad142@gmail.com>
2023-03-05 16:42:21 +0200

    use s3 to store tfstate

--- provider.tf
+++ provider.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
     region = var.region
 }
+
+# store the state file in S3 bucket
+terraform {
+    backend "s3" {
+    bucket = "bucket-mariam"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
