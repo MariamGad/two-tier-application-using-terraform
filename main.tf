ac00c724783a2b95ff0385907503621b8095f3de
MariamGad <mariamgad142@gmail.com>
2023-03-05 02:22:58 +0200

    module dependency

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ module "network"{
     route_table_name=var.route_table_name
     internet_gw_name=var.internet_gw_name
 }
+
+module "ec2"{
+    source="../modules/application"
+    instance_type=var.instance_type
+    ami=var.ami
+    tag_name=var.tag_name
+    subnet_id=module.network.subnet_id
+    depends_on = [
+    module.network
+  ]  
+}
